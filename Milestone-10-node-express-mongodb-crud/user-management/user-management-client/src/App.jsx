04e0995958b7b9729b061b3a28287e05a9956c51
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-10-30 22:19:58 +0600

    ...

---
+++ Milestone-10-node-express-mongodb-crud/user-management/user-management-client/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Suspense } from 'react'
+import './App.css'
+import Users from './components/Users'
+const userPromise=fetch('http://localhost:3000/users').then(res=>res.json())
+function App() {
+
+
+  return (
+    <>
+      <h1>Users Management</h1>
+     <Suspense fallback={<p>Loading users...</p>}>
+        <Users userPromise={userPromise} />
+      </Suspense>
+    </>
+  )
+}
+
+export default App
