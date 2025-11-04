3b7cf09d98afc6657806c6eb8912ccf1a04675ad
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-11-04 19:57:11 +0600

    ..

---
+++ Milestone-10-node-express-mongodb-crud/3G-model/Server/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port =process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+// mongodb
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://model-db:KNRMp6b5SHoJViL5@cluster0.x08ux7h.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const db = client.db('model-db')
+    const modelCollection = db.collection('models')
+
+   app.get('/models',async(req,res)=>{
+    const result = await modelCollection.find().toArray()
+   
+    res.send(result)
+    
+   })
+
+
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
